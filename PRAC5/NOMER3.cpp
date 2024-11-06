c3dac853fa7aceb9d05f6d620ea1e1966a15a096
dangyyyy <gbeorg24@gmail.com>
2024-11-06 09:12:55 +0300

    Update NOMER3.cpp

--- PRAC5/NOMER3.cpp
+++ PRAC5/NOMER3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
@@ -7,31 +7,23 @@ int main() {
     int n;
     cout << "Введите количество строк: ";
     cin >> n;
-    cin.ignore(); // Чтобы убрать символ новой строки после ввода числа
-
-    vector<string> strings; 
-
-    cout << "Введите строки:\n";
+    cin.ignore();
+    vector<string> strings(n);
     for (int i = 0; i < n; i++) {
-        string str;
-        getline(cin, str);
-        strings.push_back(str); 
-
-        for (int j = strings.size() - 1; j > 0; j--) {
-            if (strings[j] < strings[j - 1]) {
-                string temp = strings[j];
-                strings[j] = strings[j - 1];
-                strings[j - 1] = temp;
-            }
-            else {
-                break;
+        cout << "Введите строку: ";
+        getline(cin, strings[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strings[j] > strings[j + 1]) {
+                swap(strings[j], strings[j + 1]);
             }
         }
     }
 
-    cout << "Отсортированные строки:\n";
-    for (int i = 0; i < strings.size(); i++) {
-        cout << strings[i] << endl;
+    cout << "Отсортированные строки:" << endl;
+    for (const string& s : strings) {
+        cout << s << endl;
     }
 
     return 0;
