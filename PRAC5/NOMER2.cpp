99956fc8d8ec3d73829ff2cdc997142f768eca5c
dangyyyy <gbeorg24@gmail.com>
2024-11-06 09:15:19 +0300

    Update NOMER2.cpp

--- PRAC5/NOMER2.cpp
+++ PRAC5/NOMER2.cpp
@@ -1,26 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n;
     cout << "Введите количество элементов: ";
     cin >> n;
-
     int* elements = new int[n];
     cout << "Введите элементы: ";
     for (int i = 0; i < n; i++) {
         cin >> elements[i];
     }
-
     int max = *elements;
     for (int i = 1; i < n; i++) {
         if (*(elements + i) > max) {
             max = *(elements + i);
         }
     }
-
     cout << "Максимум: " << max << endl;
-
     delete[] elements;
     return 0;
 }
