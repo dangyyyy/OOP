eb09f3e4755444820a9505242c7ee9ae92d2c463
dangyyyy <gbeorg24@gmail.com>
2024-11-06 09:14:55 +0300

    Update NOMER1.cpp

--- PRAC5/NOMER1.cpp
+++ PRAC5/NOMER1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -6,20 +6,17 @@ int main() {
     int n;
     cout << "Введите количество элементов: ";
     cin >> n;
-
     vector<int> elements(n);
     cout << "Введите элементы: ";
     for (int i = 0; i < n; i++) {
         cin >> elements[i];
     }
-
     int max = elements[0];
     for (int i = 1; i < n; i++) {
         if (elements[i] > max) {
             max = elements[i];
         }
     }
-
     cout << "Максимум: " << max << endl;
     return 0;
 }
