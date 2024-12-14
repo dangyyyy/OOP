63433cef27dfd0b4072afed0a4b8253ee0289b14
dangyyyy <gbeorg24@gmail.com>
2024-12-14 21:38:20 +0300

    Update BaseStore.h

--- PRAC8/BaseStore.h
+++ PRAC8/BaseStore.h
@@ -3,13 +3,10 @@
 #include <string>
 using namespace std;
 
-// Абстрактный класс для всех типов магазинов/складов
 class BaseStore {
 public:
-    // Чисто виртуальная функция для вывода деталей
     virtual void printDetails() const = 0;
 
-    // Новый метод для получения типа объекта
     virtual string getType() const = 0;
 
     virtual ~BaseStore() = default;
