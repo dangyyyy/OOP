23904f2635161e5bfe9f265ac5837a9df9bf8dbb
dangyyyy <gbeorg24@gmail.com>
2024-12-14 21:38:51 +0300

    Update NOMER.cpp

--- PRAC8/NOMER.cpp
+++ PRAC8/NOMER.cpp
@@ -5,18 +5,15 @@
 using namespace std;
 
 int main() {
-    // Обычный магазин
     Store store("Продуктовый рай", "ул. Мира, 5", "8:00 - 22:00");
     store.printDetails();
 
     cout << endl;
 
-    // Склад
     Warehouse warehouse1(500), warehouse2(300);
     warehouse1.printDetails();
     warehouse2.printDetails();
 
-    // Сравнение складов
     if (warehouse1 == warehouse2)
         cout << "Склады равны по вместимости.\n";
     else
@@ -24,19 +21,16 @@ int main() {
 
     cout << endl;
 
-    // Логистический магазин
     LogisticsStore logistics1("Склад-магазин", "ул. Заводская, 7", "9:00 - 18:00", 1000);
     LogisticsStore logistics2("Склад-Север", "ул. Транспортная, 10", "8:00 - 20:00", 500);
     logistics1.printDetails();
     logistics2.printDetails();
 
-    // Объединение логистических магазинов
     LogisticsStore mergedLogistics = logistics1 + logistics2;
     mergedLogistics.printDetails();
 
     cout << endl;
 
-    // Демонстрация перегрузки операторов с магазином
     store += Store("Магазин у дома", "ул. Ленина, 10", "9:00 - 21:00");
     store.printDetails();
 
